7b4c2769f657896a6e9df007d833ab42e1dcdf9d
Lee Weisberger <lee.weisberger@duke.edu>
2014-02-22 14:24:10 -0500

    added lexer that works for Constants and Commands

---
+++ src/Parser/Lexer.java
@@ -0,0 +1,69 @@
+package Parser;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Lexer {
+    public static enum TokenType {
+        // Token types cannot have underscores
+        CONSTANT("-?[0-9]+\\.?[0-9]*"), COMMAND("[a-zA-z_]+(\\?)?"), WHITESPACE("[ \t\f\r\n]+");
+
+        public final String pattern;
+
+        private TokenType(String pattern) {
+            this.pattern = pattern;
+        }
+    }
+
+    public static class Token {
+        public TokenType type;
+        public String data;
+
+        public Token(TokenType type, String data) {
+            this.type = type;
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%s %s)", type.name(), data);
+        }
+    }
+
+    public static ArrayList<Token> lex(String input) {
+        // The tokens to return
+        ArrayList<Token> tokens = new ArrayList<Token>();
+
+        // Lexer logic begins here
+        StringBuffer tokenPatternsBuffer = new StringBuffer();
+        for (TokenType tokenType : TokenType.values())
+            tokenPatternsBuffer.append(String.format("|(?<%s>%s)", tokenType.name(), tokenType.pattern));
+        Pattern tokenPatterns = Pattern.compile(new String(tokenPatternsBuffer.substring(1)));
+
+        // Begin matching tokens
+        Matcher matcher = tokenPatterns.matcher(input);
+        while (matcher.find()) {
+            if (matcher.group(TokenType.CONSTANT.name()) != null) {
+                tokens.add(new Token(TokenType.CONSTANT, matcher.group(TokenType.CONSTANT.name())));
+                continue;
+            } else if (matcher.group(TokenType.COMMAND.name()) != null) {
+                tokens.add(new Token(TokenType.COMMAND, matcher.group(TokenType.COMMAND.name())));
+                continue;
+            } else if (matcher.group(TokenType.WHITESPACE.name()) != null)
+                continue;
+        }
+
+        return tokens;
+    }
+    
+
+    public static void main(String[] args) {
+        String input = "fd 11.5 rt 45";
+
+        // Create tokens and print them
+        ArrayList<Token> tokens = lex(input);
+        for (Token token : tokens)
+            System.out.println(token);
+    }
+}
