0e35270d9dc5c52c088d9f17168fce53aeb27e60
Lee Weisberger <lee.weisberger@duke.edu>
2014-02-23 18:53:36 -0500

    implemented setxy and setHeading

---
+++ src/commands/SetHeading.java
@@ -0,0 +1,9 @@
+package commands;
+
+public class SetHeading {
+
+	public SetHeading() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/commands/SetXY.java
@@ -0,0 +1,9 @@
+package commands;
+
+public class SetXY {
+
+	public SetXY() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
