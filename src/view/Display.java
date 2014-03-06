e4ed1d77f6cb94c505559da77c118a76cbe19c42
Steve Wang <sw211@duke.edu>
2014-03-06 05:42:13 -0500

    everything latest

--- src/view/Display.java
+++ src/view/Display.java
@@ -27,53 +27,53 @@ import java.awt.event.KeyEvent;
 import jgame.platform.JGEngine;
 
 public class Display extends JPanel implements ChangeListener{
-	private DisplayTab tab1; 
-	private DisplayTab tab2; 
-	private DisplayTab tab3; 
-	private static Model myModel;
-	private static TurtleGraphicsWindow turtleGrahicsWindow = new TurtleGraphicsWindow(); 
-	private JTabbedPane tabbedPane; 
-	private int selectedIndex;
+    private DisplayTab tab1; 
+    private DisplayTab tab2; 
+    private DisplayTab tab3; 
+    private static Model myModel;
+    private static TurtleGraphicsWindow turtleGrahicsWindow = new TurtleGraphicsWindow(); 
+    private JTabbedPane tabbedPane; 
+    private int selectedIndex;
 
-	public Display(Model model, String language) {
-		super(new GridLayout(1, 1));
+    public Display(Model model, String language) {
+        super(new GridLayout(1, 1));
 
-		tabbedPane = new JTabbedPane();
-		
-		tab1 = new DisplayTab(model, null); 
-		tab1.setTurtleGraphicsWindow(turtleGrahicsWindow);
+        tabbedPane = new JTabbedPane();
 
-		tab2 = new DisplayTab(new Model(), null); 
+        tab1 = new DisplayTab(model, "English"); 
+        tab1.setTurtleGraphicsWindow(turtleGrahicsWindow);
+        tab2 = new DisplayTab(new Model(), "English"); 
+        tab3 = new DisplayTab(new Model(), "English");
 
-		tab3 = new DisplayTab(new Model(), null);
+        tabbedPane.addTab("tab 1", null, tab1, "first workspace");
+        tabbedPane.addTab("tab 2", null, tab2, "second workspace");
+        tabbedPane.addTab("tab 3", null, tab3, "second workspace");
+        tabbedPane.addChangeListener(this); 
+        //Add the tabbed pane to this panel.
+        add(tabbedPane);
 
+        //The following line enables to use scrolling tabs.
+        tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+    }
 
-		tabbedPane.addTab("tab 1", tab1);
-		tabbedPane.addTab("tab 2", tab2);
-		tabbedPane.addTab("tab 3", tab3);
-		tabbedPane.addChangeListener(this); 
-		//Add the tabbed pane to this panel.
-		add(tabbedPane);
+    public DisplayTab getTab1() {
+        return tab1;
+    }
 
-		//The following line enables to use scrolling tabs.
-		tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
-	}
-
-	public DisplayTab getTab1() {
-		return tab1;
-	}
-	
-	public static TurtleGraphicsWindow getTurtleGrahicsWindow() {
-		return turtleGrahicsWindow;
-	}
+    public static TurtleGraphicsWindow getTurtleGrahicsWindow() {
+        return turtleGrahicsWindow;
+    }
 
 
-	public void stateChanged(ChangeEvent e){
-		 if ( tabbedPane.getSelectedIndex() != selectedIndex ) { 
-	        ((DisplayTab) tabbedPane.getSelectedComponent()).setTurtleGraphicsWindow(turtleGrahicsWindow);
-	        selectedIndex = tabbedPane.getSelectedIndex();
-	        }
-	}
-
-
+    public void stateChanged(ChangeEvent e){
+        if ( tabbedPane.getSelectedIndex() != selectedIndex ) { 
+            ((DisplayTab) tabbedPane.getSelectedComponent()).setTurtleGraphicsWindow(turtleGrahicsWindow);
+            selectedIndex = tabbedPane.getSelectedIndex();
+        }
+    }
+/*
+    for effective switch btw tabs:
+        http://examples.javacodegeeks.com/desktop-java/swing/jtabbedpane/get-set-selected-tab-in-jtabbedpane/
+    */
+    
 }
